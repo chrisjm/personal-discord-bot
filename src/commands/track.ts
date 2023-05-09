2df9ee3260bc4727950cc8921a7957bd87751d1e
Chris J Mears <chris.mears@gmail.com>
2023-05-09 15:50:20 -0700

    Update water tracker commands

--- src/commands/track.ts
+++ src/commands/track.ts
@@ -12,6 +12,32 @@ import {
 } from "discord.js";
 
 const apiUrl = process.env.WATER_TRACKER_API_URL;
+const CODE_START = "```\n";
+const CODE_END = "```\n";
+
+interface WaterTrackerEntry {
+  id: { S: string };
+  entry_datetime: { S: string };
+  milliliters: { N: string };
+}
+
+function getDateOffsetUTCString(offset = 0) {
+  let datetime = new Date();
+  datetime.setDate(datetime.getDate() + offset);
+
+  let dateUTC = new Date(datetime.toUTCString());
+  // Set the start of the day to midnight UTC
+  dateUTC.setHours(0, 0, 0, 0);
+  const startOfDayUTC = new Date(dateUTC.getTime());
+  // Set the end of the day to 11:59:59.999 PM UTC
+  dateUTC.setHours(23, 59, 59, 999);
+  const endOfDayUTC = new Date(dateUTC.getTime());
+
+  return {
+    startOfDayUTC,
+    endOfDayUTC,
+  };
+}
 
 export const data = new SlashCommandBuilder()
   .setName("track-water")
@@ -21,7 +47,11 @@ export const data = new SlashCommandBuilder()
       .setName("action")
       .setDescription("What would you like to do?")
       .setRequired(true)
-      .addChoices({ name: "Add", value: "add" })
+      .addChoices(
+        { name: "Add entry", value: "add" },
+        { name: "Show today's entries", value: "today" },
+        { name: "Show yesterday's entries", value: "yesterday" }
+      )
   );
 
 export async function execute(interaction: ChatInputCommandInteraction) {
@@ -90,7 +120,53 @@ export async function execute(interaction: ChatInputCommandInteraction) {
         components: [],
       });
     }
+  } else if (interaction.options.getString("action") === "today") {
+    const { startOfDayUTC, endOfDayUTC } = getDateOffsetUTCString(0);
+    try {
+      const response = await axios.get(
+        `${apiUrl}/range?start=${startOfDayUTC}&end=${endOfDayUTC}`
+      );
+      const { data } = response;
+      const items: WaterTrackerEntry[] = data.Items;
+      const entries = items.map((item) => {
+        const datetime = new Date(item.entry_datetime.S);
+        return `${datetime.toLocaleString()} - ${item.milliliters.N}mL`;
+      });
+      const totalMl = items.reduce((acc, curr) => acc + +curr.milliliters.N, 0);
+      await interaction.reply({
+        content: `${CODE_START}${entries.join(
+          "\n"
+        )}\nToday's total water intake: ${totalMl}mL\n${CODE_END}`,
+      });
+    } catch (e) {
+      console.error(e);
+      await interaction.editReply({ content: `Error! ${e}` });
+    }
+  } else if (interaction.options.getString("action") === "yesterday") {
+    const { startOfDayUTC, endOfDayUTC } = getDateOffsetUTCString(-1);
+    try {
+      const response = await axios.get(
+        `${apiUrl}/range?start=${startOfDayUTC}&end=${endOfDayUTC}`
+      );
+      const { data } = response;
+      const items: WaterTrackerEntry[] = data.Items;
+      const entries = items.map((item) => {
+        const datetime = new Date(item.entry_datetime.S);
+        return `${datetime.toLocaleString()} - ${item.milliliters.N}mL`;
+      });
+      const totalMl = items.reduce((acc, curr) => acc + +curr.milliliters.N, 0);
+      await interaction.reply({
+        content: `${CODE_START}${entries.join(
+          "\n"
+        )}\nYesterday's total water intake: ${totalMl}mL\n${CODE_END}`,
+      });
+    } catch (e) {
+      console.error(e);
+      await interaction.editReply({ content: `Error! ${e}` });
+    }
   } else {
-    await interaction.reply(`not implemented`);
+    await interaction.reply(
+      `${interaction.options.getString("action")} not implemented`
+    );
   }
 }
