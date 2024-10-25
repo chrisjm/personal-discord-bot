792393346b2781767abbcc9b09cf2ed416e42042
Chris Mears <chris.mears@gmail.com>
2024-10-24 23:32:58 -0700

    Add News Minimalist news feed

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,29 @@
-import { Client, Events } from "discord.js";
+import { Client, Events, TextChannel } from "discord.js";
+import {
+  displayNewEntries,
+  fetchRSSFeed,
+  storeNewEntries,
+} from "../news-minimalist";
 
 export default {
   name: Events.ClientReady,
   once: true,
   execute(client: Client) {
     console.log(`🟢 Ready! Logged in as ${client.user?.tag}`);
+
+    const channel = client.channels.cache.get(
+      process.env.NEWS_MINIMALIST_DISCORD_CHANNEL_ID
+    ) as TextChannel;
+
+    // Fetch RSS feed and display new entries every hour
+    setInterval(async () => {
+      const items = await fetchRSSFeed(
+        process.env.NEWS_MINIMALIST_RSS_FEED_URL
+      );
+      if (items.length > 0) {
+        await storeNewEntries(items);
+        displayNewEntries(channel);
+      }
+    }, 1000); // Run every 1 hour
   },
 };
